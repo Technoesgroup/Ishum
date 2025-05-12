9d14f4833a9fb53ef4700f92d183472dbb62f2b8
harshrajput177 <harshrajput30411@gmail.com>
2025-05-12 16:14:55 +0530

    update some files

--- EcommerceSite/src/Component/BestSeller/BestSellerBanner.jsx
+++ EcommerceSite/src/Component/BestSeller/BestSellerBanner.jsx
@@ -1,14 +1,15 @@
 import React  from "react";
 import '../../Style-CSS/BestSeller-css/BestSellerBanner.css';
 import img1  from '../../images/Banner6.png';
+import img2 from '../../images/Banner5.png';
 
 const Banner = ()=>{
 
     return(
        <div className="banner-container">
          <div  className="Banner-image">
-           <img src={img1} alt="" />
-
+           <img src={img1} alt=""   className="banner6"/>
+           <img src={img2} alt=""  className="banner5" />
         </div>
        </div>
     )
