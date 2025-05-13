46defa05af62dc9eda59bf884d0e307c874736d3
harshrajput177 <harshrajput30411@gmail.com>
2025-05-13 14:51:24 +0530

    update some files

--- backend/index.js
+++ backend/index.js
@@ -88,7 +88,7 @@ app.post("/verify-payment", (req, res) => {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
     const body = razorpay_order_id + "|" + razorpay_payment_id;
-    const expectedSignature = crypto.createHmac("sha256", "FXqRgaBmdvJDjVzN28PAlgGl")
+const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
         .update(body.toString())
         .digest("hex");
 
