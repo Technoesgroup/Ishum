599f7d42149c041d1ed85085b5d004e010c96bcd
harshrajput177 <harshrajput30411@gmail.com>
2025-05-18 12:03:01 +0530

    update some files

---
+++ backend/Config/CloudConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dy2ppgch9",
+  api_key: "698946116748184",
+  api_secret: "afWinR4X2x-igf3FZ2RizX802JA",
+});
+
+module.exports = cloudinary;
