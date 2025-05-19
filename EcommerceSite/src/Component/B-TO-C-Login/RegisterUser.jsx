9e6a205ea5f7c967988e5ce23fca1bc005cb36b0
harshrajput177 <harshrajput30411@gmail.com>
2025-05-19 13:36:14 +0530

    update some files

--- EcommerceSite/src/Component/B-TO-C-Login/RegisterUser.jsx
+++ EcommerceSite/src/Component/B-TO-C-Login/RegisterUser.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import "../B-TO-C-Login/RegisterUser.css";
 import img1 from '../../images/18839a14eab62a1c7d6277c6f8ba14f8.png';
 import OtpVerification from "./OtpVerification";
+import GoogleIcon from '@mui/icons-material/Google';
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -113,7 +114,7 @@ const SignupForm = ({ setShowB2UModal, setShowLoginModal }) => {
                 <img src="https://cdn-icons-png.flaticon.com/512/0/747.png" alt="Apple" />
               </button>
               <button className="RegisterUser-social-btn">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google" />
+                <GoogleIcon />
               </button>
               <button className="RegisterUser-social-btn">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" />
