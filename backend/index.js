d62d6a3e09aaaa84399a1f74e77285b5a8018331
harshrajput177 <harshrajput30411@gmail.com>
2025-05-19 13:51:44 +0530

    update some  files

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
   'https://ishum.in',
   // 'https://ishum-frontend.onrender.com',
   'http://localhost:5173',
-  'http://localhost:5174'
+  'http://localhost:5174',
+  'http://admin.ishum.in/'
 ];
 
 app.use(cors({
