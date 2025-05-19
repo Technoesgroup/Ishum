0f5acbefe1aa79c3c78f1273968d3aa12aded2c4
harshrajput177 <harshrajput30411@gmail.com>
2025-05-19 13:36:14 +0530

    update some files

--- EcommerceSite/src/Component/B-TO-C-Login/LoginUser.jsx
+++ EcommerceSite/src/Component/B-TO-C-Login/LoginUser.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../B-TO-C-Login/LoginUser.css";
 import img1 from "../../images/18839a14eab62a1c7d6277c6f8ba14f8.png";
 import OtpVerification from "./OtpVerification";
+import GoogleIcon from '@mui/icons-material/Google';
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -91,10 +92,7 @@ const SignupForm = ({ setShowB2UModal, setShowLoginModal }) => {
                   <img src="https://cdn-icons-png.flaticon.com/512/0/747.png" alt="Apple" />
                 </button>
                 <button className="LoginUser-social-btn">
-                  <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-                    alt="Google"
-                  />
+                    <GoogleIcon />
                 </button>
                 <button className="LoginUser-social-btn">
                   <img
