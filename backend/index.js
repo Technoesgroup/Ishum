a2da57a710bc3258d9f51ccf09f9f1e9549e83d4
harshrajput177 <harshrajput30411@gmail.com>
2025-05-21 23:30:41 +0530

    update some files

--- backend/index.js
+++ backend/index.js
@@ -114,4 +114,4 @@ const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_S
 
 
 
-app.listen(PORT,  '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
